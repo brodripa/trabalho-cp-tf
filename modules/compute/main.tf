6f278b02d14f8e5636bc85a2581fa6fed62991d6
brodripa <biancarodrigues_paulino@hotmail.com>
2026-06-16 13:33:36 -0300

    main

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,5 +1,6 @@
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-security-group"
+# Security Group para o Load Balancer (Este sim fica aberto para a internet)
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-security-group"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -14,6 +15,61 @@ resource "aws_security_group" "ec2_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Modifique o SG das EC2 para aceitar APENAS o SG do Load Balancer
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_sg.id] # Segurança fina aqui
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "app_lb" {
+  name               = "infra-app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = var.subnet_ids # Ele vai usar as duas subnets públicas que você criou
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "infra-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
 
@@ -42,8 +98,27 @@ resource "aws_autoscaling_group" "app_asg" {
   max_size            = 4
   min_size            = 1
 
+  # ADICIONE ESTA LINHA:
+  target_group_arns   = [aws_lb_target_group.app_tg.arn]
+
   launch_template {
     id      = aws_launch_template.app_template.id
     version = "$Latest"
+  }
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "meutrabalhoiac.com"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www.meutrabalhoiac.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app_lb.dns_name
+    zone_id                = aws_lb.app_lb.zone_id
+    evaluate_target_health = true
   }
 }
